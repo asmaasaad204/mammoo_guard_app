8841f78b572c30312df8135afca1f081c56c4c65
LENOVO <saadasmaa204@gmail.com>
2023-09-27 19:26:14 +0300

    initial project. added empty home screen.

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:facebook/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: Locale('en'),
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+    );
+  }
+}
